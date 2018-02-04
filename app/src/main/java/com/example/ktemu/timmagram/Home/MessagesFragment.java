0ed78e50911b38f1dc527f5ddad6aec00eee072f
Temurhon Kamariddinov <temurhon.dev@gmail.com>
2018-02-04 19:06:59 +0000

    Cleaned up and added icons at the top

---
+++ app/src/main/java/com/example/ktemu/timmagram/Home/MessagesFragment.java
@@ -0,0 +1,26 @@
+package com.example.ktemu.timmagram.Home;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ktemu.timmagram.R;
+
+/**
+ * Created by ktemu on 04/02/2018.
+ */
+
+public class MessagesFragment extends Fragment {
+    private static final String TAG = "MessagesFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_messages,container,false);
+
+        return view;
+    }
+}
